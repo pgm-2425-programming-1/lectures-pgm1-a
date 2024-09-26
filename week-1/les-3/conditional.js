10d2d381dfd5b48ad8f6ce7000ea4a7b4bc59d9f
Michael Vanderpoorten <michael@yungpanda.com>
2024-09-26 10:40:18 +0200

    W1 - Les3

---
+++ week-1/les-3/conditional.js
@@ -0,0 +1,26 @@
+// if - else
+let x = 22;
+
+if (x > 20) {
+  console.log("X is groter dan 20");
+}
+if (x < 20) {
+  console.log("X is kleiner dan 20");
+}
+if (x === 20) {
+  console.log("X is gelijk aan 20");
+}
+
+if (x > 20) {
+  console.log("X is groter dan 20");
+} else {
+  console.log("X is kleiner of gelijk aan 20");
+}
+
+if (x > 20) {
+  console.log("X is groter dan 20");
+} else if (x < 20) {
+  console.log("X is kleiner dan 20");
+} else {
+  console.log("X is gelijk aan 20");
+}
---
+++ week-1/les-3/herhaling.js
@@ -0,0 +1,20 @@
+// data types
+const brand = "Volkswagen"; // string
+const model = "ID Buzz";
+const year = 2023; // number
+const isCar = true; // boolean;
+let x; // undefined
+
+// ID Buzz is een auto van Volkswagen
+// --------------------------------
+// deze is gemaakt in het jaar 2023
+
+console.log(model + " is een auto van " + brand);
+console.log("-------------------------");
+console.log("deze is gemaakt in het jaar " + year);
+
+console.log(`
+${model} is een auto van ${brand}
+---------------------------------
+deze is gemaakt in het jaar ${year}
+`);
